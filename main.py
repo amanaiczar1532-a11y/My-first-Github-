04b1f329114c29af6a7801b442e33fa23299eef9
amanaiczar1532-a11y <amanaiczar1532@gmail.com>
2026-03-15 13:51:04 +0800

    Add files via upload
    1st code

---
+++ main.py
@@ -0,0 +1,16 @@
+choice = int(input("enter choice 1 - 4  "))
+if choice == 1:
+    print ("Aman")
+if choice == 2:
+    fruits = ["apple", "banana", "mango"]
+    print(fruits)
+if choice == 3:
+    for i in range (1,10+1):
+        print (i)
+if choice == 4:
+    first_no = int(input("enter first no "))
+    second_no = int(input("enter second no "))
+    def add (a,b):
+        sum = a+b
+        print(sum)
+    add(first_no,second_no)
